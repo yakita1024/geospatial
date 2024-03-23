9c84e5672a0be68887b572cec4f8f4a7e6b12229
yakita1024 <yakita1024@gmail.com>
2024-03-22 20:23:21 -0700

    change on development branch

--- sample_file.py
+++ sample_file.py
@@ -5,3 +5,7 @@ import geopandas as gpd
 
 # Set path
 project_dir = r'C:\project'
+
+# Chane for development branch
+# Read shapefile
+test_gdf = gpd.read_file(os.path.join(project_dir, 'test.shp'))
